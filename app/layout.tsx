0f9d33830cc4df6dc2ab3c4d690315bf96c90b21
Michael Owuor <michael.owuor@gmail.com>
2024-03-08 20:49:50 +0300

    feat(layout): update metadata title and description
    The metadata title and description in the layout component have been updated to provide a more descriptive and accurate representation of the application. The new title is "pichafy.ai - AI Powered Image Generator" and the description is "Artificial Intelligence powered image generator."

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-	title: "pichafy.ai",
+	title: "pichafy.ai - AI Powered Image Generator",
 	description: "Artificicial Intelligence powered image generator.",
 };
 
