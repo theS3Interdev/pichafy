54daf280f3d99474623010c604becde087c71e51
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-03-08 11:56:28 +0300

    feat: add image, transaction, and user actions (#5)
    * feat: add mongodb and mongoose dependencies
    
    This commit adds the "mongodb" and "mongoose" dependencies to the project.
    
    * feat: Add image, transaction, and user models
    
    This commit adds the image, transaction, and user models to the codebase. The image model defines the structure of an image object with various properties such as title, transformation type, public ID, secure URL, etc. The transaction model represents a transaction with properties like creation date, stripe ID, amount, plan, credits, and buyer. Lastly, the user model defines the structure of a user object with properties like clerk ID, email address, username, photo URL, first name, last name, plan ID and credit balance.
    
    The commit also includes a new file `mongoose.ts` which contains a function `connectToDatabase` that establishes a connection to MongoDB using the provided MONGODB_URL environment variable.
    
    These changes are necessary for implementing functionality related to images and transactions in the application.
    
    * feat: add image, transaction, and user actions
    
    This commit adds new files for image, transaction, and user actions. It also includes functions for creating, reading, updating, and deleting users. Additionally, there is a function for updating user credits.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,158 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+/* eslint-disable prefer-const */
+
+/* eslint-disable no-prototype-builtins */
+
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import qs from "qs";
+
+import { aspectRatioOptions } from "@/lib/constants";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+/* error handler */
+export const handleError = (error: unknown) => {
+	if (error instanceof Error) {
+		/* this is a native JavaScript error (e.g., TypeError, RangeError) */
+		console.error(error.message);
+		throw new Error(`Error: ${error.message}`);
+	} else if (typeof error === "string") {
+		/* this is a string error message */
+		console.error(error);
+		throw new Error(`Error: ${error}`);
+	} else {
+		/* this is an unknown type of error */
+		console.error(error);
+		throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+	}
+};
+
+/* placeholder loader - while image is transforming */
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#7986AC" offset="20%" />
+      <stop stop-color="#68769e" offset="50%" />
+      <stop stop-color="#7986AC" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#7986AC" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+	typeof window === "undefined"
+		? Buffer.from(str).toString("base64")
+		: window.btoa(str);
+
+export const dataUrl = `data:image/svg+xml;base64,${toBase64(
+	shimmer(1000, 1000)
+)}`;
+
+/* form url query */
+export const formUrlQuery = ({
+	searchParams,
+	key,
+	value,
+}: FormUrlQueryParams) => {
+	const params = { ...qs.parse(searchParams.toString()), [key]: value };
+
+	return `${window.location.pathname}?${qs.stringify(params, {
+		skipNulls: true,
+	})}`;
+};
+
+/* remove key from query */
+export function removeKeysFromQuery({
+	searchParams,
+	keysToRemove,
+}: RemoveUrlQueryParams) {
+	const currentUrl = qs.parse(searchParams);
+
+	keysToRemove.forEach((key) => {
+		delete currentUrl[key];
+	});
+
+	/* remove null or undefined values */
+	Object.keys(currentUrl).forEach(
+		(key) => currentUrl[key] == null && delete currentUrl[key]
+	);
+
+	return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+}
+
+/* debounce */
+export const debounce = (func: (...args: any[]) => void, delay: number) => {
+	let timeoutId: NodeJS.Timeout | null;
+	return (...args: any[]) => {
+		if (timeoutId) clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => func.apply(null, args), delay);
+	};
+};
+
+/* get imgafe size */
+export type AspectRatioKey = keyof typeof aspectRatioOptions;
+export const getImageSize = (
+	type: string,
+	image: any,
+	dimension: "width" | "height"
+): number => {
+	if (type === "fill") {
+		return (
+			aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+			1000
+		);
+	}
+	return image?.[dimension] || 1000;
+};
+
+/* doanload image */
+export const download = (url: string, filename: string) => {
+	if (!url) {
+		throw new Error("Resource URL not provided. You need to provide one.");
+	}
+
+	fetch(url)
+		.then((response) => response.blob())
+		.then((blob) => {
+			const blobURL = URL.createObjectURL(blob);
+			const a = document.createElement("a");
+			a.href = blobURL;
+
+			if (filename && filename.length)
+				a.download = `${filename.replace(" ", "_")}.png`;
+			document.body.appendChild(a);
+			a.click();
+		})
+		.catch((error) => console.log({ error }));
+};
+
+/* deep merge objects */
+export const deepMergeObjects = (obj1: any, obj2: any) => {
+	if (obj2 === null || obj2 === undefined) {
+		return obj1;
+	}
+
+	let output = { ...obj2 };
+
+	for (let key in obj1) {
+		if (obj1.hasOwnProperty(key)) {
+			if (
+				obj1[key] &&
+				typeof obj1[key] === "object" &&
+				obj2[key] &&
+				typeof obj2[key] === "object"
+			) {
+				output[key] = deepMergeObjects(obj1[key], obj2[key]);
+			} else {
+				output[key] = obj1[key];
+			}
+		}
+	}
+
+	return output;
+};
