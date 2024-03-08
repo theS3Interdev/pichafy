dfab30b38b7ba0893e37fa1008f46a561a610235
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-03-08 20:42:32 +0300

    feat: Update custom field and transformation form components (#9)
    * feat(ui): Add new components for alert dialog, form, input, label, pagination, select
    
    - Added a new component `AlertDialog` for displaying alert dialogs.
    - Added a new component `Form` for creating forms with form context.
    - Added a new component `Input` for rendering input fields.
    - Added a new component `Label` for rendering labels.
    - Added a new component `Pagination` for pagination functionality.
    - Added a new component `Select` for creating select dropdowns.
    
    These components provide reusable UI elements that can be used in various parts of the application.
    
    * feat(image): add, update, delete, and get image functions
    
    feat(transaction): checkout credits and create transaction functions
    
    * feat: update page.tsx and add TransformationForm component
    
    This commit updates the code in page.tsx by removing the UserButton component and adds a new TransformationForm component. The changes improve the functionality of the page.
    
    The commit also introduces a new Header component that can be used to display titles and subtitles.
    
    * feat: add custom field component
    
    feat: add insufficient credits modal component
    
    feat: add media uploader component
    
    feat: add transformation form component
    
    feat: add transformed image component
    
    * feat(components): add Collection component
    
    This commit adds the Collection component, which displays a list of images with pagination and search functionality.
    
    * feat: Update custom field and transformation form components
    
    - Updated the import statements in `custom-field.tsx` to match the new file structure.
    - Modified the formLabel text in `transformation-form.tsx` to capitalize certain words for consistency.
    - Added a new button element in `transformation-form.tsx`.
    
    These changes improve code organization and enhance user experience.

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,38 @@
-const AddTransformationTypePage = () => {
-	return <div>Add Transformation Type Page</div>;
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
+import { transformationTypes } from "@/lib/constants";
+
+import { getUserById } from "@/lib/actions/user.actions";
+
+import { Header } from "@/components/shared/header";
+import { TransformationForm } from "@/components/shared/transformation-form";
+
+const AddTransformationTypePage = async ({
+	params: { type },
+}: SearchParamProps) => {
+	const { userId } = auth();
+
+	const transformation = transformationTypes[type];
+
+	if (!userId) redirect("/sign-in");
+
+	const user = await getUserById(userId);
+
+	return (
+		<>
+			<Header title={transformation.title} subtitle={transformation.subTitle} />
+
+			<section className="mt-10">
+				<TransformationForm
+					action="Add"
+					userId={user._id}
+					type={transformation.type as TransformationTypeKey}
+					creditBalance={user.creditBalance}
+				/>
+			</section>
+		</>
+	);
 };
 
 export default AddTransformationTypePage;
