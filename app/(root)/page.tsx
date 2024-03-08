1b04e77ddca4b81a60df39a9be96f61be4c479b6
Michael Owuor <michael.owuor@gmail.com>
2024-03-08 14:39:53 +0300

    feat: update page.tsx and add TransformationForm component
    This commit updates the code in page.tsx by removing the UserButton component and adds a new TransformationForm component. The changes improve the functionality of the page.
    
    The commit also introduces a new Header component that can be used to display titles and subtitles.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const HomePage = () => {
 	return (
 		<div>
 			<p>Home Page</p>
-
-			<UserButton afterSignOutUrl="/" />
 		</div>
 	);
 };
---
+++ components/shared/transformation-form.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const TransformationForm = () => {
+	return <div>Transformation Form</div>;
+};
